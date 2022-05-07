0378e961a6cfcd5876d73dc2010bf453c223164c
Andrey Miskov <amiskov@gmail.com>
2022-05-07 19:24:01 +0300

    Annotate file

--- postgres_to_es/backoff.py
+++ postgres_to_es/backoff.py
@@ -1,9 +1,12 @@
 import logging
 from functools import wraps
 from time import sleep
+from typing import Callable
 
 
-def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
+def backoff(start_sleep_time: float = 0.1,
+            factor: int = 2,
+            border_sleep_time: int = 10) -> Callable:
     """
     Функция для повторного выполнения функции через некоторое время, если
     возникла ошибка. Использует наивный экспоненциальный рост времени повтора
@@ -18,9 +21,9 @@ def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
     :return: результат выполнения функции
     """
 
-    def func_wrapper(func):
+    def func_wrapper(func: Callable) -> Callable:
         @wraps(func)
-        def inner(*args, **kwargs):
+        def inner(*args, **kwargs) -> Callable:
             sleep_time = start_sleep_time
             n = 1
 
